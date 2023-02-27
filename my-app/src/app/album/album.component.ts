7a734224703d147e85a5dc1f833959327b8c362b
Cristian Rivera <adbel.rivera@unitec.edu>
2023-02-26 23:37:45 -0600

    comit
    se modifico la vista de las imagenes en el componente Album

--- my-app/src/app/album/album.component.ts
+++ my-app/src/app/album/album.component.ts
@@ -9,17 +9,17 @@ export class AlbumComponent {
   title="Autos HN"
   cd: ICard={
     titulo: "Todo Terreno",
-    descripcion: "Autos 4x4, listos para la aventura",
+    descripcion: "Autos 4x4: listos para la aventura",
     enlace: "https://www.laprensa.hn/autos/4x4-el-club-de-los-mas-fuertes-ATLP698239#image-1"
     }
   cd2: ICard ={
     titulo: "Autos Deportivos",
-    descripcion: "Autos deportivos que roban miradas",
+    descripcion: "Autos Deportivos: que roban miradas",
     enlace: "https://www.autospublicidad.com/hn/images/autos/1701/thumbnail/1602202329.jpg"
     }
     cd3: ICard ={
       titulo: "Autos Clasico",
-      descripcion: "Autos Clasicos con mas de 40 anios rodando",
+      descripcion: "Autos Clasicos: mas experiencia",
       enlace: "https://i.ytimg.com/vi/yf2JZHEWN8M/maxresdefault.jpg"
       }
     
